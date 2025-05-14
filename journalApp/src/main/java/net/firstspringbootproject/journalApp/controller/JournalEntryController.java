0d37d99cc2d4fff151313cd30dbf1427718de48a
Nikhil <jainnikhil7746@gmail.com>
2025-05-14 17:40:13 +0530

    REST API CREATED

---
+++ journalApp/src/main/java/net/firstspringbootproject/journalApp/controller/JournalEntryController.java
@@ -0,0 +1,41 @@
+package net.firstspringbootproject.journalApp.controller;
+
+import net.firstspringbootproject.journalApp.entity.JournalEntry;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+@RequestMapping("/journal")
+public class JournalEntryController {
+
+    private HashMap<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @GetMapping
+    public List<JournalEntry> getAll() {
+        return new ArrayList<>(journalEntries.values());
+    }
+
+    @PostMapping
+    public boolean createEntry(@RequestBody JournalEntry myEntry){
+        journalEntries.put(myEntry.getId(),myEntry);
+        return true;
+    }
+
+    @GetMapping("id/{myId}")
+    public JournalEntry getJournalEntryById(@PathVariable Long myId){
+        return journalEntries.get(myId);
+    }
+
+    @DeleteMapping("id/{myId}")
+    public JournalEntry deleteJournalEntryById(@PathVariable Long myId){
+        return journalEntries.remove(myId);
+    }
+
+    @PutMapping("id/{myId}")
+    public JournalEntry updateEntryById(@PathVariable Long myId,@RequestBody JournalEntry myEntry){
+        return journalEntries.put(myId, myEntry);
+    }
+}
